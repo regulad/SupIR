f284f4cc6dd81c1e46b61e273204012b01fe0c9a
Parker Wahle <regulad@regulad.xyz>
2024-11-23 12:32:40 -0500

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "xyz.regulad.supir"
         minSdk = 21
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
